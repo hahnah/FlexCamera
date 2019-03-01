6c703a22d70e00d92b52a4c15209712bc56b374f
hahnah <superhahnah@gmail.com>
2019-03-01 16:30:40 +0900

    Initial Commit

---
+++ FlexCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlexCamera
+//
+//  Created by Natsuki HARAI on 2019/03/01.
+//  Copyright © 2019年 hahnah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
